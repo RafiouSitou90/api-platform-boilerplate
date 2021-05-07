1bcb64552032ecc8be15db2ca1ce92abf9f4da17
Rafiou Sitou <rafiousitou90@gmail.com>
2021-05-07 14:37:40 -0300

    feat(update-openapi-doc): Update OpenApi doc adding OpenApiFactory.php

---
+++ src/OpenApi/OpenApiFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\OpenApi;
+
+use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\Core\OpenApi\Model\PathItem;
+use ApiPlatform\Core\OpenApi\OpenApi;
+
+class OpenApiFactory implements OpenApiFactoryInterface
+{
+    public function __construct(private OpenApiFactoryInterface $decorated)
+    {
+    }
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        $openApi = $this->decorated->__invoke($context);
+        /** @var PathItem $path */
+        foreach ($openApi->getPaths()->getPaths() as $key => $path) {
+            if ($path->getGet() && 'hidden' === $path->getGet()->getSummary()) {
+                $openApi->getPaths()->addPath($key, $path->withGet(null));
+            }
+        }
+
+        return $openApi;
+    }
+}
